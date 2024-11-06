ea6ee72cebbfe5b2e9be5e2a0d92ddab55202f25
l3itisys <leiticia.tlb@gmail.com>
2024-11-06 12:49:58 -0500

    Implement aircraft data structures and simulation class

---
+++ build/CMakeFiles/aircraft_sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aircraft_sim.dir/src/sim/aircraft.cpp.o"
+  "CMakeFiles/aircraft_sim.dir/src/sim/aircraft.cpp.o.d"
+  "CMakeFiles/aircraft_sim.dir/src/sim/channel_manager.cpp.o"
+  "CMakeFiles/aircraft_sim.dir/src/sim/channel_manager.cpp.o.d"
+  "aircraft_sim"
+  "aircraft_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aircraft_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
