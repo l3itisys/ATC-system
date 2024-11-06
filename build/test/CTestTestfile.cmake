7213c1bd4d8a524482733d83f4b537199d792e0a
l3itisys <leiticia.tlb@gmail.com>
2024-11-06 14:26:05 -0500

    Updated .gitignore and removed build directory from git tracking

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tycia/Documents/Github/ATC-system/test
-# Build directory: /home/tycia/Documents/Github/ATC-system/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AircraftTest "/home/tycia/Documents/Github/ATC-system/build/test/runTests")
-set_tests_properties(AircraftTest PROPERTIES  _BACKTRACE_TRIPLES "/home/tycia/Documents/Github/ATC-system/test/CMakeLists.txt;11;add_test;/home/tycia/Documents/Github/ATC-system/test/CMakeLists.txt;0;")
